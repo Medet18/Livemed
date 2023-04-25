75a30e2b7fcc80122d3d4d6f0fed913e3de3db7d
Medet <medetkurganbaev18@.com>
2023-04-25 14:50:39 +0600

    blade files

---
+++ public/doctor/medicinesDoctor.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+
+    <link rel="stylesheet" href="../css/medicinesDoctor.css">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+
+</head>
+<body>
+<header class="header">
+    <div class="container">
+        <div class="logo">
+            <a href="#" id="logo" class="logo" onclick="window.location.href='indexDoctor.htm'"> <span>Live</span>MedKZ </a>
+        </div>
+
+        <div class="login-btn">
+            <button class="button" onclick="window.location.href='loginDoctor.htm'">login</button>
+        </div>
+
+
+    </div>
+</header>
+
+<section class="search">
+    <div class="container">
+        <div class="poisk">
+            <form action="#" class="price-form">
+                <input type="text" class="price-input" name="medicine" id="" placeholder="Medicine">
+                <button class="button">search</button>
+            </form>
+        </div>
+
+        <div class="client">
+            <a href="#">
+                <div class="client-name">
+                    <h2>1</h2>
+                    <h2>Aspirin</h2>
+                    <h2>A02BA01</h2>
+                    <h2>39182201</h2>
+                    <button class="button">add</button>
+                    <button class="btn">delete</button>
+                </div>
+            </a>
+            <a href="#">
+                <div class="client-name">
+                    <h2>1</h2>
+                    <h2>Aspirin</h2>
+                    <h2>A02BA01</h2>
+                    <h2>39182201</h2>
+                    <button class="button">add</button>
+                    <button class="btn">delete</button>
+                </div>
+            </a>
+            <a href="#">
+                <div class="client-name">
+                    <h2>1</h2>
+                    <h2>Aspirin</h2>
+                    <h2>A02BA01</h2>
+                    <h2>39182201</h2>
+                    <button class="button">add</button>
+                    <button class="btn">delete</button>
+                </div>
+            </a>
+        </div>
+        <!--        <div class="res">-->
+        <!--            <div class="title">-->
+        <!--                <h1>The name of product</h1>-->
+        <!--                <p>Product №1</p>-->
+        <!--                <p>Product №1</p>-->
+        <!--                <p>Product №1</p>-->
+        <!--            </div>-->
+        <!--            <div class="title">-->
+        <!--                <h1>The number</h1>-->
+        <!--                <p>5<span>шт</span></p>-->
+        <!--                <p>5<span>шт</span></p>-->
+        <!--                <p>5<span>шт</span></p>-->
+        <!--            </div>-->
+        <!--            <div class="title">-->
+        <!--                <h1>The dosage</h1>-->
+        <!--                <p>13<span>mg</span></p>-->
+        <!--                <p>13<span>mg</span></p>-->
+        <!--                <p>13<span>mg</span></p>-->
+        <!--            </div>-->
+        <!--            <div class="title">-->
+        <!--                <h1>The graphic</h1>-->
+        <!--                <p>open</p>-->
+        <!--                <p>open</p>-->
+        <!--                <p>open</p>-->
+        <!--            </div>-->
+        <!--        </div>-->
+
+    </div>
+</section>
+
+
+
+<section class="new" id="new">
+    <div class="container">
+        <div class="news">
+            <h2 class="sub-title">Новости LiveMedKZ !</h2>
+            <div class="news-text">Будьте в курсе новостей с LiveMedKZ</div>
+            <button class="button" type="button" id="price-action" style="max-width: 250px;">Начать -></button>
+            <div class="circ"></div>
+            <div class="circ2"></div>
+        </div>
+    </div>
+</section>
+
+<footer class="footer">
+    <div class="container">
+        <div class="logo">
+            <a href="#" class="logo"> @ 2023 LiveMedKZ </a>
+        </div>
+
+        <div class="rights">
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-github"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-linkedin"></i>
+        </div>
+
+    </div>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script src="../js/logo.js"></script>
+</body>
+</html>
